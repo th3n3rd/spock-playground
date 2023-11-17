03db9eeeedbd727bf0bcc318928977ae185ace0a
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-11-17 19:16:22 +0000

    First commit

---
+++ src/test/java/com/example/spockplayground/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spockplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
