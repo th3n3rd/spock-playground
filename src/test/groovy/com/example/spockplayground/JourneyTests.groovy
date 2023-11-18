dcce7ccace47463138a0c595fcbedfedf321b741
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-11-18 11:35:31 +0000

    Extract the player DSL for the journey test

--- src/test/groovy/com/example/spockplayground/JourneyTests.groovy
+++ src/test/groovy/com/example/spockplayground/JourneyTests.groovy
@@ -1,6 +1,6 @@
 package com.example.spockplayground
 
-import groovy.transform.Canonical
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -26,38 +26,4 @@ class JourneyTests extends Specification {
         player.hasWon()
     }
 
-    static class Player {
-
-        private NewGame currentGame;
-        private final TestRestTemplate client
-
-        Player(TestRestTemplate client) {
-            this.client = client
-        }
-
-        void startNewGame() {
-            def response = client.postForEntity("/games", null, NewGame)
-            assert response.statusCode.is2xxSuccessful()
-            currentGame = response.body
-        }
-
-        void guess(String word) {
-            def response = client.postForEntity("/games/{id}/guesses", [word: word], Void, currentGame.id)
-            assert response.statusCode.is2xxSuccessful()
-        }
-
-        boolean hasWon() {
-            def response = client.getForEntity("/games/{id}", GameDetails, currentGame.id)
-            assert response.statusCode.is2xxSuccessful()
-            return response.body.won
-        }
-
-        static class NewGame {
-            UUID id
-        }
-
-        static class GameDetails {
-            boolean won
-        }
-    }
 }
---
+++ src/test/groovy/com/example/spockplayground/Player.groovy
@@ -0,0 +1,38 @@
+package com.example.spockplayground
+
+import org.springframework.boot.test.web.client.TestRestTemplate
+
+class Player {
+
+    private Player.NewGame currentGame;
+    private final TestRestTemplate client
+
+    Player(TestRestTemplate client) {
+        this.client = client
+    }
+
+    void startNewGame() {
+        def response = client.postForEntity("/games", null, Player.NewGame)
+        assert response.statusCode.is2xxSuccessful()
+        currentGame = response.body
+    }
+
+    void guess(String word) {
+        def response = client.postForEntity("/games/{id}/guesses", [word: word], Void, currentGame.id)
+        assert response.statusCode.is2xxSuccessful()
+    }
+
+    boolean hasWon() {
+        def response = client.getForEntity("/games/{id}", Player.GameDetails, currentGame.id)
+        assert response.statusCode.is2xxSuccessful()
+        return response.body.won
+    }
+
+    static class NewGame {
+        UUID id
+    }
+
+    static class GameDetails {
+        boolean won
+    }
+}
