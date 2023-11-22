96a4c89d500c253052615e731512d73029e42695
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-11-22 22:29:40 +0000

    Mark events in order to enhance the auto-generated docs

--- src/main/java/com/example/spockplayground/gameplay/GameWon.java
+++ src/main/java/com/example/spockplayground/gameplay/GameWon.java
@@ -1,5 +1,7 @@
 package com.example.spockplayground.gameplay;
 
 import java.util.UUID;
+import org.jmolecules.event.annotation.DomainEvent;
 
+@DomainEvent
 public record GameWon(UUID id, String playerId, int attempts) {}
--- src/main/java/com/example/spockplayground/gameplay/GuessMade.java
+++ src/main/java/com/example/spockplayground/gameplay/GuessMade.java
@@ -1,5 +1,7 @@
 package com.example.spockplayground.gameplay;
 
 import java.util.UUID;
+import org.jmolecules.event.annotation.DomainEvent;
 
+@DomainEvent
 public record GuessMade(UUID id, String playerId, int attempts) {}
